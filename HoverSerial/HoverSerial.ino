e8a28e3a74bc0d9339e867b202ef18db49050bc9
Zinc <minkevicius.linas@gmail.com>
2019-02-12 11:45:28 +0200

    Update HoverSerial.ino
    Added read data. NOT TESTED

--- HoverSerial/HoverSerial.ino
+++ HoverSerial/HoverSerial.ino
@@ -12,6 +12,7 @@ Serialcommand cmd;
 void setup() {
   // initialize serial:
   Serial1.begin(115200);// A10 A9 USART1 Port of Blue pill
+  Serial2.begin(115200);
   Serial3.begin(115200);// USART3 port of blue pill for out comms.
   // reserve 200 bytes for the inputString:
 
@@ -22,7 +23,7 @@ void setup() {
 
 void loop() {
   // print the string when a newline arrives:
-  if (Serial1.available()) { //Enter your speed and steer separated by space
+  if (Serial1.available()>2) { //Enter your speed and steer separated by space
     Serial1.println("Hello");
     cmd.steer = Serial1.parseInt();
     cmd.speed = Serial1.parseInt();
@@ -33,6 +34,14 @@ void loop() {
     Serial1.flush();
 
   }
+  if(Serial2.available())
+  {Serial1.println("Got Hover data");
+    while (Serial2.available()) { //Enter your speed and steer separated by space
+    Serial1.print(Serial2.read());
+
+  }
+   Serial1.println();}
+
   Serial3.write((uint8_t*)&cmd.steer, sizeof(cmd.steer)); //Off it goes
   Serial3.write((uint8_t*)&cmd.speed, sizeof(cmd.speed));
   delay(20);
